47495cd18220a96c94c7d4d25c1d702f7dd3cf59
Gabriel <gabriel.amaral414@hotmail.com>
2024-09-14 20:50:47 -0300

    Ok

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const emailDeUsuario = document.getElementById("nomeDeUsuarioLogin");
 
 
 
-formularioLogin.addEventListener('submit', function(event){
+formularioLogin.addEventListener('submit', function (event) {
     event.preventDefault();
-    
+
     console.log(senhaDeUsuario.value);
     console.log(nomeDeUsuario.value);
     console.log("bombou");
@@ -31,20 +31,20 @@ console.log("ai dentro?");
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
+function myFunction(id) {
+    document.getElementById(id).classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
     if (!event.target.matches('.botaoDrop')) {
-      var dropdowns = document.getElementsByClassName("divConteudoDropdown");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
         }
-      }
     }
-  }
+}
